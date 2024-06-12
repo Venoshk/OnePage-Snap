c2471bbde99b86505c9a46509ab2a98466a339f9
Henrique <venoshk.henrique19@gmail.com>
2024-06-11 23:10:21 -0300

    Adicionando Menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,27 @@
-import './Header.css'
-import './Responsividade.css'
-import Logo from "../../assets/images/logo.svg"
-import Todo from "../../assets/images/icon-todo.svg"
-import Calendar from "../../assets/images/icon-calendar.svg"
-import Reminders from "../../assets/images/icon-reminders.svg"
-import Planning from "../../assets/images/icon-planning.svg"
-import Menu     from "../../assets/images/icon-menu.svg"
+import  { useState } from 'react';
+import './Header.css';
+import './Responsividade.css';
+import Logo from "../../assets/images/logo.svg";
+import Todo from "../../assets/images/icon-todo.svg";
+import Calendar from "../../assets/images/icon-calendar.svg";
+import Reminders from "../../assets/images/icon-reminders.svg";
+import Planning from "../../assets/images/icon-planning.svg";
+import Menu from "../../assets/images/icon-menu.svg";
+import Close from "../../assets/images/icon-close-menu.svg";
+
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <>
       <header>
         <img src={Logo} alt="Logo" />
-        <nav className="content-nav">
-          <ul className='hist'>
+        <nav className={`content-nav ${menuOpen ? 'open' : ''}`}>
+          <ul>
             <li className="dropdown">
               Features
               <ul className="dropdown-menu">
@@ -38,13 +47,15 @@ function Header() {
             <a href="#" className="login">Login</a>
             <a href="#" className="register">Register</a>
           </div>
-         
         </nav>
         
-        <img src={Menu} alt="" />
+        <img src={menuOpen ? Close : Menu} alt="Menu" className="menu-icon" onClick={toggleMenu} />
+
+        
       </header>
+      {menuOpen && <div className='border' onClick={toggleMenu}></div>}
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
